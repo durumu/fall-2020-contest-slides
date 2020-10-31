fe19aead160dc1c78945a19965d1cb024ced4ce2
nghiepchinhluu <51274335+nghiepchinhluu@users.noreply.github.com>
2020-10-31 00:20:20 -0500

    Added solution slides + .cpp for I - I want candy!

--- I.cpp
+++ I.cpp
@@ -1,36 +1,82 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
-  cin.tie(0)->sync_with_stdio(0);
-  cin.exceptions(cin.failbit);
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int n,m,d;
+	cin >> n >> m >> d;
+	vector<int> h(n),p(n),t(n),s(m), count(m);
+	for (int i =0;i<n;i++) cin >> h[i];
+	for (int i =0;i<n;i++) cin >> p[i];
+	for (int i =0;i<n;i++) 
+	{
+		cin >> t[i];
+		t[i] -= 1;
+		count[t[i]] +=1;
+	}
+	for (int i =0;i<m;i++) cin >> s[i];
 
-  int n, m;
-  cin >> n >> m;
+	// Preprocessing to put the array in order
+	vector<pair<int,pair<int,int>>> b(n);
+	for (int i =0;i<n;i++)
+	{
+		b[i] = make_pair(t[i],make_pair(h[i],p[i]));
+	}
+	sort(b.begin(),b.end());
 
-  vector<string> rows(n), cols(m);
-  for (int i = 0; i < n; ++i) cin >> rows[i];
-  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) {
-    cols[j].push_back(rows[i][j]);
-  }
+	vector<pair<int,int>> a;
+	vector<int> jump;
+	
+	a.push_back(make_pair(0,0));
+	jump.push_back(0);
+	
+	int lastType = 0;
+	int sumHappiness = 0;
+	for (int i =0;i<n;i++)
+	{
+		int type = b[i].first;
+		int happiness = b[i].second.first;
+		int price = b[i].second.second;
+		if (type != lastType)
+		{
+			a.push_back(make_pair(sumHappiness,s[lastType]));
+			jump.push_back(count[lastType]+1);
+			sumHappiness = 0;
+			lastType = type;
+		}
+		a.push_back(make_pair(happiness,price));
+		jump.push_back(1);
+		sumHappiness += happiness;
+	}
+	a.push_back(make_pair(sumHappiness,s[lastType]));
+	jump.push_back(count[lastType]+1);
 
-  auto seq = [&](string &s) {
-    int cnt = 0, ans = 1e9;
-    for (int i = 0; i < int(s.size()); ++i) {
-      if (s[i] == '.') {
-        ++cnt;
-      } else {
-        if (cnt) ans = min(ans, cnt);
-        cnt = 0;
-      }
-    }
-    if (cnt) ans = min(ans, cnt);
-    return ans;
-  };
 
-  int ans = 1e9;
-  for (auto s: rows) ans = min(ans, seq(s));
-  for (auto s: cols) ans = min(ans, seq(s));
-  cout << ans << '\n';
+	// DP
+	vector<vector<int>> dp(a.size(),vector<int>(d+1));
+	for (int i=0;i<a.size();i++)
+	{
+		for (int j=1;j<=d;j++) dp[i][j] = -1;
+		dp[i][0] = 0;
+	}
+	int res = 0;
+
+	for (int i =1;i<a.size();i++)
+	{
+		int k = i - jump[i];
+		int happiness = a[i].first;
+		int price = a[i].second;
+				
+		for (int j =0;j <= d;j++)
+		{		
+			dp[i][j] = dp[i-1][j];
+			if (j-price>=0 && dp[k][j-price]!=-1)
+			{
+				dp[i][j] = max(dp[i][j],dp[k][j-price] + happiness);
+			}
+			res = max(res, dp[i][j]);
+		}
+	}
+	cout << res << endl;
 }
