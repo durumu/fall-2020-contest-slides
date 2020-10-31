f0713679a1a23d88f310f3a94fbd8ca4fe44815a
Julia Graham <julia.graham@protonmail.com>
2020-10-31 14:12:06 -0500

    added C and F

---
+++ F.py
@@ -0,0 +1,25 @@
+legend = {'.':'grass','#':'city','M':'mountain','T':'tree'}
+
+n = int(input())
+A = [input() for _ in range(n)]
+
+rot, mirror = 0, 0
+
+for _ in range(int(input())):
+    command = input()
+    if command in ['clockwise', 'counterclockwise']:
+        if (command == 'counterclockwise') ^ mirror:
+            rot = (rot + 1) % 4  # CCW, or mirrored CW
+        else:
+            rot = (rot + 3) % 4  # CW, or mirrored CCW
+    elif command in ['flip', 'mirror']:
+        mirror ^= 1
+        if command == 'flip':
+            rot = (rot + 2) % 4
+    else:
+        i, j = map(int, command.split()[1:])
+        if mirror:
+            j = n - j - 1
+        for _ in range(rot):
+            i, j = j, n - i - 1
+        print(legend[A[i][j]])
